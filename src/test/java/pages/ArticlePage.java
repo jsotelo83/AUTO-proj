74f01e21b715a7755a8c18679ad5f903f5d3671e
Sotelo <JSotelo@luxoft.com>
2019-03-07 09:53:42 -0600

    New changes to locators and some fixes in classes

--- src/test/java/pages/ArticlePage.java
+++ src/test/java/pages/ArticlePage.java
@@ -14,31 +14,31 @@ public class ArticlePage extends BasePage {
         super(driver);
     }
 
-    By articleTitle = By.id("firstHeading");
-    By studioAlbumsRow = By.xpath("//*[contains(text(), \"Studio albums\")]/../td");
-    By reputationStudioAlbumLink = By.xpath("//*[contains(text(), \"Studio albums\")]/../td//*[contains(text(), \"Reputation\")]");
-    By popUp = By.className("mwe-popups");
+    public static By articleTitle = By.id("firstHeading");
+    public static By studioAlbumsRow = By.xpath("//*[contains(text(), \"Studio albums\")]/../td");
+    public static By reputationStudioAlbumLink = By.xpath("//*[contains(text(), \"Studio albums\")]/../td//*[contains(text(), \"Reputation\")]");
+    public static By popUp = By.className("mwe-popups");
 
-    public String getPageTitle(){
+    public static String getPageTitle(){
         return driver.getTitle();
     }
 
-    public String getArticleTitle(){
+    public static String getArticleTitle(){
         WebDriverWait wait = new WebDriverWait(driver, 10);
         WebElement element = wait.until(
                 ExpectedConditions.visibilityOfElementLocated(articleTitle));
         return element.getText();
     }
 
-    public void validateExpectedArticle(String articleName){
-        String actualPageTitle = this.getPageTitle();
-        String actualArticleTitle = this.getArticleTitle();
+    public static void validateExpectedArticle(String articleName){
+        String actualPageTitle = ArticlePage.getPageTitle();
+        String actualArticleTitle = ArticlePage.getArticleTitle();
 
         Assert.assertTrue(actualPageTitle.contains(articleName), "The page title is different than expected");
         Assert.assertTrue(actualArticleTitle.contains(articleName), "The article title is different than expected");
     }
 
-    public void validateExpectedStudioAlbums(String[] albums){
+    public static void validateExpectedStudioAlbums(String[] albums){
         String albumsString = readText(studioAlbumsRow);
         System.out.print(albumsString);
         Boolean flag = true;
@@ -52,7 +52,7 @@ public class ArticlePage extends BasePage {
         Assert.assertTrue(flag, "The albums don't match");
     }
 
-    public void validateHoverMessageAppears(){
+    public static void validateHoverMessageAppears(){
         WebElement link = driver.findElement(reputationStudioAlbumLink);
 
         click(studioAlbumsRow);
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,27 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
-    public WebDriver driver;
+    public static WebDriver driver;
 
     public BasePage (WebDriver driver){
         this.driver = driver;
     }
 
-    String baseURL = "https://www.wikipedia.org/";
+    static String baseURL = "https://www.wikipedia.org/";
 
-    public void goTo (String URL){
+    public static void goTo (String URL){
         driver.get(URL);
     }
 
-    public void click (By element) {
+    public static void click (By element) {
         driver.findElement(element).click();
     }
 
-    public void writeText (By element, String text) {
+    public static void writeText (By element, String text) {
         driver.findElement(element).sendKeys(text);
     }
 
-    public String readText (By element) {
+    public static String readText (By element) {
         return driver.findElement(element).getText();
     }
 }
